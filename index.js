522c3c12189f8bcef10762ae8797e425d9dd5bb9
ab1123 <asarma626@gmail.com>
2024-11-08 12:19:35 +0530

    Updated Cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors=require("cors")
 const articleRoutes = require("./routes/articleRoutes");
 require("dotenv");
 
 const app = express();
+app.use(cors())
+
 
 
 app.use(express.json());
