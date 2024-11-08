4cd40e0a4e9caec69cac29ec6b4c25d28e9fd0ae
Siddharth <moonmars2au@gmail.com>
2024-11-09 00:25:45 +0530

    djsgdujsgd

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors=require("cors")
-const articleRoutes = require("./routes/articleRoutes");
-require("dotenv");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const articleRoutes = require('./routes/articleRoutes');
+require('dotenv');
 
 const app = express();
-app.use(cors())
-
-
+app.use(
+  cors({
+    origin: 'http://localhost:4321',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
 app.use(express.json());
-app.use("/api", articleRoutes);
+app.use('/api', articleRoutes);
 
 const PORT = process.env.PORT || 3000;
 mongoose
-  .connect("mongodb+srv://onlyforcocindia:OCbWfLiYTVGXi7e1@hackernation.9xavz.mongodb.net/?retryWrites=true&w=majority&appName=Hackernation")
+  .connect(
+    'mongodb+srv://onlyforcocindia:OCbWfLiYTVGXi7e1@hackernation.9xavz.mongodb.net/?retryWrites=true&w=majority&appName=Hackernation'
+  )
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
-  .catch((err) => console.log("Failed to connect to MongoDB", err));
+  .catch((err) => console.log('Failed to connect to MongoDB', err));
